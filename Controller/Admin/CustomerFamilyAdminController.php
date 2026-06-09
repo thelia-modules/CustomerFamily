3a5a390f175c986efde1bbc9d09d4284f54c960c
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-09 12:02:33 +0200

    Revert "fix: render module config page with twig template extension"
    This reverts commit a6d54156a33050a2c7dad05495e40918ca888313.

--- Controller/Admin/CustomerFamilyAdminController.php
+++ Controller/Admin/CustomerFamilyAdminController.php
@@ -148,7 +148,7 @@ class CustomerFamilyAdminController extends BaseAdminController
             $brandRestrictions[$customerFamily->getId()] = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         }
 
-        return $this->render("customer_family_module_configuration.html.twig", [
+        return $this->render("customer_family_module_configuration", [
             'categoryRestrictions' => $categoryRestrictions,
             'brandRestrictions' => $brandRestrictions,
             'families' => $families,
